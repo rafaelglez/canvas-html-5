c9f38c9a8171e8635d9503e732dd8c1f4f8759fe
rafaelglez <rafael.glez@gmail.com>
2015-02-09 05:18:24 -0600

    Pixel Collition
    Clear interval to stop execution after collition

--- js/ch4-pixelCollitions.js
+++ js/ch4-pixelCollitions.js
@@ -47,8 +47,8 @@ function canvasApp(){
 		context.clearRect(0,0,canvas.width, canvas.height);
 		context.drawImage(blueObject.image, blueObject.x, blueObject.y);
 		context.drawImage(redObject.image, redObject.x, redObject.y);
-		console.log("redObject.redImageData.data[3]=" +	redObject.redImageData.data[3]);
-		console.log("blueObject.blueImageData.data[3]=" + blueObject.blueImageData.data[3]);
+		//console.log("redObject.redImageData.data[3]=" +	redObject.redImageData.data[3]);
+//		console.log("blueObject.blueImageData.data[3]=" + blueObject.blueImageData.data[3]);
 		if (boundingBoxCollide(blueObject, redObject)){
 			console.log("bounding box collide");
 			console.log("redObject.redImageData.data[3]=" +	redObject.redImageData.data[3]);
@@ -59,12 +59,15 @@ function canvasApp(){
 			var yMax = Math.min( blueObject.y+blueObject.height,redObject.y+redObject.height );
 			for ( var pixelX = xMin; pixelX < xMax; pixelX++ ) {
 				for ( var pixelY = yMin; pixelY < yMax; pixelY++ ) {
-					var bluepixel = ((pixelX-blueObject.x ) + (pixelY-blueObject.y )*blueObject.width )*4 + 3 ;
-					var redpixel = ((pixelX-redObject.x) +	(pixelY-redObject.y)*redObject.width)*4 + 3 ;
+					var bluepixel = ((pixelX-blueObject.x + pixelY-blueObject.y )*blueObject.width )*4 + 3 ;
+					var redpixel = ((pixelX-redObject.x + pixelY-redObject.y)*redObject.width)*4 + 3 ;
 					if (( blueObject.blueImageData.data [ bluepixel ] !== 0 ) && ( redObject.redImageData.data[ redpixel ] !== 0 )) {
 						console.log("pixel collision");
+						console.log("bluepixel " + bluepixel);
+						console.log("redpixel " + redpixel);
 						blueObject.dx=0;
 						redObject.dx=0;
+						window.clearInterval(startUp);
 						break;
 					}
 				}
