7ead56e2ce99a62c0d43de74dd5ead7df53fffe5
rafaelglez <rafael.glez@gmail.com>
2015-03-04 06:49:58 -0600

    Bezier curve variable speed
    improvements:

--- js/ch5-11-bezierCurve.js
+++ js/ch5-11-bezierCurve.js
@@ -19,27 +19,19 @@ function canvasApp(){
 	var p1 = {x:70, y:200};
 	var p2 = {x:125, y:295};
 	var p3 = {x:350, y:350};
-	var ball = {x:0, y:0, speed:.01, t:0};
+	var spd =  0.005
+	var npoints = Math.floor(1/spd);   //speed = 1/npoints
+	var ball = {x:0, y:0, speed:spd, t:0};
 	var tx, ty;
 	var bcPoints = Array();
-	bcPoints.push({x:60, y:10});
+	var i = 0;
+
 	// Add event listener to keyword
 	document.addEventListener("keydown",keyDownListener,false);
 	
-	function gameLoop() {
-		loop = window.setTimeout(gameLoop, 20);
-		drawScreen();
-	}
-	gameLoop();
-
-	function drawScreen () {	
-		if (bcPoints.length <= 101){
-		update();
-		}
-		render();
-	}
+	init();
 	
-	function update() {
+	function init(){	
 		var	t = ball.t;
 		var cx = 3 * (p1.x - p0.x)
 		var bx = 3 * (p2.x - p1.x) - cx;
@@ -47,15 +39,38 @@ function canvasApp(){
 		var cy = 3 * (p1.y - p0.y);
 		var by = 3 * (p2.y - p1.y) - cy;
 		var ay = p3.y - p0.y - cy - by;
-		xt = Math.floor(ax*(t*t*t) + bx*(t*t) + cx*t + p0.x);
-		yt = Math.floor(ay*(t*t*t) + by*(t*t) + cy*t + p0.y)
-			ball.t += ball.speed;
-		if (ball.t > 1) {
-			ball.t = 1;
-		}
-		if (bcPoints.length <= 101)
+		
+		//calculate points	
+		while (bcPoints.length <= npoints) {
+			xt = Math.floor(ax*(t*t*t) + bx*(t*t) + cx*t + p0.x);
+			yt = Math.floor(ay*(t*t*t) + by*(t*t) + cy*t + p0.y)
+			t = ball.t += ball.speed;
 			bcPoints.push({x:xt, y:yt});
-		Debugger.log("t: " + ball.t);
+		}
+	
+		//start
+		gameLoop();
+	}
+	
+	function gameLoop() {
+		loop = window.setTimeout(gameLoop, 20);
+		drawScreen();
+	}
+	
+	function drawScreen () {	
+		update();
+		render();
+	}
+	
+	function update(){
+		if(i == npoints+1){
+			i=0;
+		bcPoints.reverse()
+		}
+		xt = bcPoints[i].x;
+		yt = bcPoints[i].y;
+	//Debugger.log("bcPoints: "+ "["+i+"]"+ bcPoints[i].x +" "+bcPoints[i].y);
+	i++;
 	}
 	
 	function render() {
@@ -95,20 +110,11 @@ function canvasApp(){
 		context.fillStyle = "#FFFFFF";
 		context.fillText("3",p3.x-2, p3.y+2);
 		//Draw circle moving
-		if(ball.t < 1){
-			context.fillStyle = "#000000";
-			context.beginPath();
-			context.arc(xt,yt,5,0,Math.PI*2,true);
-			context.closePath();
-			context.fill();
-		}
-		else{
-			context.fillStyle = "#000000";
-			context.beginPath();
-			context.arc(xt,yt,5,0,Math.PI*2,true);
-			context.closePath();
-			context.fill();
-		}
+		context.fillStyle = "#000000";
+		context.beginPath();
+		context.arc(xt,yt,5,0,Math.PI*2,true);
+		context.closePath();
+		context.fill();
 	}
 		
 	
