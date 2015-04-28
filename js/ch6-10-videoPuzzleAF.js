10bf3af2cd7b3acc7646605b1955a346e89d86e7
rafaelglez <rafael.glez@gmail.com>
2015-04-27 19:03:58 -0500

    Puzzle Video
    Character encoding added

--- js/ch6-10-videoPuzzleAF.js
+++ js/ch6-10-videoPuzzleAF.js
@@ -7,27 +7,26 @@ function eventWindowLoaded() {
 	videoDiv = document.createElement('div');
 	document.body.appendChild(videoDiv);
 	videoDiv.appendChild(videoElement);
-	//videoDiv.setAttribute("style", "display:none;");
+	videoDiv.setAttribute("style", "display:none;");
 	var videoType = supportedVideoFormat(videoElement);
 	if (videoType == "") {
 		alert("no video support");
 	return;
 	}
 	videoElement.addEventListener("canplaythrough",videoLoaded,false);
-videoElement.setAttribute("src", "http://quiet.pcriot.com/video/bailar" + "." + videoType);
+	videoElement.setAttribute("src", "http://quiet.pcriot.com/video/bailar" + "." + videoType);
 	videoElement.setAttribute("width", 640);
 	videoElement.setAttribute("height", 360);
 }
 
 function supportedVideoFormat(video){
 	var returnExtension = "";
-	//if (video.canPlayType("video/webm") == "probably" || video.canPlayType("video/webm") == "maybe") {
-//		returnExtension = "webm";
-//	} else 
 	if(video.canPlayType("video/mp4") == "probably" || video.canPlayType("video/mp4") == "maybe") {
 		returnExtension = "mp4";
+	} else if (video.canPlayType("video/webm") == "probably" || video.canPlayType("video/webm") == "maybe") {
+		returnExtension = "webm";
 	} else if(video.canPlayType("video/ogg") =="probably" ||	video.canPlayType("video/ogg") == "maybe") {
-		returnExtension = "ogg";
+		returnExtension = "ogv";
 	}
 	return returnExtension;
 }
