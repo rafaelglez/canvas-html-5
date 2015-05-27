e4ec19ee159de0d556330e2186ebedec8c479b04
rafaelglez <rafael.glez@gmail.com>
2015-05-27 16:26:01 -0500

    IDGenerator
    static version of IDGenerator

---
+++ js/StaticIDGenerador.js
@@ -0,0 +1,22 @@
+//static version of IDGenerator
+
+function IDGenerator(){
+};
+
+IDGenerator.getRandomInt = function( min, max ) {
+			return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
+		 }
+		 
+IDGenerator.generate = function() {
+			 var timestamp = Date.now().toString();
+			 var length = 8;
+			 var partimestamp = timestamp.split( "" ).reverse();
+			 var id = "";
+			 
+			 for( var i = 0; i < length; ++i ) {
+				var index = this.getRandomInt( 0, partimestamp.length - 1 );
+				id += partimestamp[index];	 
+			 }
+			 
+			 return id;
+}	
