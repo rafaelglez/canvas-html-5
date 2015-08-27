f06cb9a757ca3fdff2d1724f70dc0bc9330b345e
rafaelglez <rafael.glez@gmail.com>
2015-08-26 19:21:12 -0500

    Rotation
    Add movement

--- js/ch8-Rotation.js
+++ js/ch8-Rotation.js
@@ -18,11 +18,18 @@ function canvasApp(){
 	var height = 20;
 	var offsetX = width / 2 ;
 	var offsetY = height / 2 ;
-	var alpha = 0.2;
+	var alpha = 1;
+	var facingX = 0;
+	var facingY = 0;
+	var movingX = 0;
+	var movingY = 0;
+	var rotationalVelocity = 5; //how many degrees to turn the ship
+	var thrustAcceleration = .03;
+	var keyPressList = [];
 	
-	//Debugger.log(canvas);
 	// Add event listener to keyword
-	document.addEventListener("keydown",keyDownListener,false);
+	document.addEventListener("keydown",keyListener,false);
+	document.addEventListener("keyup",keyListener,false);
 
 	if (!canvas || !canvas.getContext){
 		return;
@@ -40,6 +47,33 @@ function canvasApp(){
 	}
 	
 	function drawScreen() {
+	
+	// up arrow
+	if (keyPressList[38]==true){
+	//thrust
+	var angleInRadians = rotation * Math.PI / 180;
+	facingX = Math.cos(angleInRadians);
+	facingY = Math.sin(angleInRadians);
+	movingX = movingX+thrustAcceleration*facingX;
+	movingY = movingY+thrustAcceleration*facingY;
+	}
+	
+	//left arrow
+	if (keyPressList[37]==true) {
+		//rotate counterclockwise
+		rotation -= rotationalVelocity;
+	}
+	
+	//right arrow
+	if (keyPressList[39]==true) {
+		//rotate clockwise
+		rotation += rotationalVelocity;;
+	} 
+	
+	x = x+movingX;
+	y = y+movingY;	
+	
+	
 		// draw background and text
 		context.globalAlpha = 1;
 		context.fillStyle = '#000000';
@@ -72,15 +106,27 @@ function canvasApp(){
 		*/
 		
 		//using the width and height to calculate
+
 		context.strokeStyle = '#ffffff';
 		context.beginPath();
+
+		//facing right
+		context.moveTo(0-offsetX,0-offsetY);
+		context.lineTo(20-offsetX,10-offsetY);
+		context.moveTo(20-offsetX,11-offsetY);
+		context.lineTo(00-offsetX,20-offsetY);
+		context.lineTo(11-offsetX,11-offsetY);
+		context.moveTo(11-offsetX,9-offsetY);
+		context.lineTo(0-offsetX,-offsetY);
+		
+/* 		
 		context.moveTo(10-offsetX,0-offsetY);
 		context.lineTo(19-offsetX,19-offsetY);
 		context.lineTo(10-offsetX,9-offsetY);
 		context.moveTo(9-offsetX,9-offsetY);
 		context.lineTo(0-offsetX,19-offsetY);
 		context.lineTo(9-offsetX,0-offsetY);
-		
+ */		
 		
 		//draw thrust
 		/* context.moveTo(8,13);
@@ -96,8 +142,11 @@ function canvasApp(){
 		//restore context
 		context.restore(); //pop old state on to screen
 		//add rotation until ship reaches 360 degrees
-		Debugger.log("r = "+ rotation +" alpha "+alpha);
+		//Debugger.log("r = "+ rotation +" alpha "+alpha);
+		
+		function initShip(){
 		if(rotation < 360){
+			alpha = 0;
 			rotation += 3;
 			if(alpha < 1){
 				alpha = alpha + 3/360;
@@ -106,16 +155,31 @@ function canvasApp(){
 				alpha = 1;
 			}
 		}
+		}
+		
+		
 	}	
 	
 	//Event handlers
-	function keyDownListener(e){
+	function keyListener(e){
 		var key = e.keyCode;
-		Debugger.log(e.keyCode);
+		
+		console.log(key);
+		if(e.type == "keydown"){
+			if (key == 37 || key == 38 || key == 39){
+				keyPressList[key] = true;
+			}
+		}	
+		if(e.type == "keyup"){
+			if (key == 37 || key == 38 || key == 39){
+				keyPressList[key] = false;
+			}
+		}
+		
+		
 		if (key == 83){
 		clearInterval(loop);
 		}
-	}
-
-	
+		Debugger.log(keyPressList);
+	}	
 }
