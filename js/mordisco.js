e8881230e19a4a8f6ca44c4d7d9fe917f1c441fb
rafaelglez <rafael.glez@gmail.com>
2015-09-01 19:12:29 -0500

    Mordico Game
    V 0.1

--- js/mordisco.js
+++ js/mordisco.js
@@ -30,17 +30,23 @@ function mordiscoApp(){
 	var maxVelocity = 5;
 	var sqrArray = [];
 	var number = 1;
+	var changeNumber = true;
+	var numberFactor = 1;
+	var numberPosition = {};
+	var lastNumber = 1;
 	var sqrArrayLen = 0;
 	const MIN_LENGTH = 2;
+	
+	function init(){
+		
+	}
+	
 	
 	document.addEventListener("keydown",keyDownListener,false);
 
 	function drawScreen() {
-		//Debugger.log("speed: " + speed + " dx: " + dx + " dy: " + dy);
-		//Dibujar la pantalla
 		context.fillStyle = '#00f';
 		context.fillRect(0, 0, canvas.width, canvas.height);
-		
 		
 		//modificamos el valor de x y y de acuerdo a lo solicitado por el usuario
 		xPrev = x;
@@ -48,10 +54,22 @@ function mordiscoApp(){
 		x = x + (speed * dx);
 		y = y + (speed * dy); 
 		
+		if (changeNumber){
+			alert(changeNumber);
+		}
+		
+		if(changeNumber){
+			numberPosition = getNumberPosition();
+			changeNumber = false;
+		}
+		
+
+		
 		
 		// dibujamos un rectagulo en el centro de la pantalla
 		context.fillStyle = "#FF0";
 		context.beginPath();
+		
 		
 		if(x != xPrev || y != yPrev){
 			sqrArray.push([x,y,sqrWidth,sqrHeight]);
@@ -62,10 +80,11 @@ function mordiscoApp(){
 		
 		sqrArrayLen = sqrArray.length;
 		if (sqrArrayLen > MIN_LENGTH){
-			sqrArray = sqrArray.slice(sqrArray.length - (number + 1), sqrArray.length);
+			sqrArray = sqrArray.slice(sqrArray.length - (numberFactor + 1), sqrArray.length);
 		}
 		
 		Debugger.log("array len"+sqrArray.length);
+		Debugger.log("numberFactor "+numberFactor);
 		Debugger.log("number "+number);
 		
 		
@@ -75,6 +94,12 @@ function mordiscoApp(){
 
 		context.closePath();
 		context.fill();
+		context.rect(numberPosition.x,numberPosition.y,numberPosition.sqrWidth,sqrHeight);
+		context.font="15px Arial bolder";
+		context.textAlign="center";
+		context.fillText(number,numberPosition.x,numberPosition.y);
+		
+
 		
 		//identificamos centro del canvas
 		// context.moveTo(0,canvas.height/2);
@@ -98,7 +123,8 @@ function mordiscoApp(){
 		var key = e.keyCode;
 		Debugger.log(e.keyCode);
 		if (key == 187){
-			number = number + 20;
+			number++;
+			numberFactor = 20 * number;
 		}
 		// stop
 		if (key == 83){
@@ -127,4 +153,58 @@ function mordiscoApp(){
 			dy = 0;
 		}	
 	}
+	
+	//calculates number's position, return place coordinates
+	function getNumberPosition(){
+		var tempX, tempY = null;
+		var sqrNumber = []; //x, y, width, height
+		var positionOK = false;
+		var collisionDetected = false;
+		while(!positionOK){
+			tempX = Math.floor(Math.random()*(canvas.width - sqrWidth));
+			tempY = Math.floor(Math.random()*(canvas.height - sqrHeight));
+			//check if there is a collision
+			for(var i = 0, len = sqrArray.length; i < len; i++){
+				collisionDetected = collision(tempX,tempY, tempX+srqWidth, tempY+sqrHeight,sqrArray[i][0], sqrArray[i][1], sqrArray[i][0] + sqrArray[i][2], sqrArray[i][1] + sqrArray[i][3]);			
+				if(collisionDetected){
+					Debugger.log("Colisión : "+collisionDetected);
+					break;
+				}
+			}
+			positionOK = !collisionDetected;
+			Debugger.log("collisionDetected : "+ collisionDetected);
+			Debugger.log("positionOK : "+ positionOK);
+		}
+		return {x:tempX,y:tempY};
+		
+				
+	}
+	
+	function drawNumber(number,position){
+		context.strokeStyle="red";
+		//context.rect(position.x,position.y,position.x+sqrWidth,position.y+sqrHeight);
+		context.font="15px bold";
+		context.textAlign="center";
+		context.fillText(number,position.x,position.y);
+		context.stroke();
+	}
+	
+	//Detect collisions
+	function collition(x11,y11,x12,y12,x21,y21,x22,y22){
+		var collision = false;
+		
+		   xOverlap = Math.max(0, Math.min(x12,x22) - Math.max(x11,x21))
+           yOverlap = Math.max(0, Math.min(y12,y22) - Math.max(y11,y21));
+			if ( xOverlap * yOverlap ){
+				collision = true;
+			}
+		return collision;
+	}
+	
+	// Returns a random number between min (inclusive) and max (exclusive)
+	function getRandomNumber(min, max) {
+		return Math.random() * (max - min) + min;
+	}
+	
+	
 }
